9b25752ac2175f821164ea73dce042c2167c3937
admin <wangyuuwen2014@qq.com>
2020-09-12 19:29:00 +0800

    第一次提交

---
+++ src/main/java/com/Chapter142Application.java
@@ -0,0 +1,13 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chapter142Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Chapter142Application.class, args);
+    }
+
+}
